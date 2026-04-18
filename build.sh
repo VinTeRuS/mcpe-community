89233d351cdff476b747be13b519053b84c937cd
dxcvvxd <antonsmunk@gmail.com>
2026-04-18 20:57:19 +0300

    build script

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+TARGET=${1:-MinecraftClient}
+TYPE=${2:-debug}
+
+if [ "$TYPE" = "release" ]; then
+    cmake --preset release
+    cmake --build --preset release -t "$TARGET"
+elif [ "$TYPE" = "debug" ]; then
+    cmake --preset debug
+    cmake --build --preset debug -t "$TARGET"
+else
+    echo "Usage: ./build.sh [target] [debug|release]"
+    echo "  target: MinecraftClient (default) or MinecraftServer"
+    echo "  type:   debug (default) or release"
+    exit 1
+fi
